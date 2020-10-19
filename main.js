71ee4497f09daef8a3c8a7ad7e676f8bda8f6887
Macz Enikő <70892737+maczenci@users.noreply.github.com>
2020-10-19 12:38:35 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let flow = document.getElementById("flow");
+
+$.getJSON('data.json', function(json){
+    console.log(json);
+    data = json.data;
+
+let begin = '<div class="col-md-4 picture">';
+let end = '</div>';
+    
+    
+    for (let i = 0; i < data.length; i++){
+    let image = '<img src="'+data[i].pics+'">';
+    flow.innerHTML += begin + image + '<div>' + data[i].hashtag + '<br>' + data[i].like + '&#x2764;</div>' + end;
+    }
+});
